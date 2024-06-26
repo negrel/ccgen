9f174b37eecc50ed74d29d5c45133c77009112f6
Alexandre Negrel <alexandre@negrel.dev>
2024-06-26 20:07:40 +0200

    move test C files in their own dir

--- tests/ccgen.sh
+++ tests/ccgen.sh
@@ -5,16 +5,16 @@ bats_require_minimum_version 1.5.0
 TESTS_DIR="./tests"
 
 @test "ccgen on simple c file" {
-  result="$(ccgen "$TESTS_DIR/mock_file.c")"
+  result="$(ccgen "$TESTS_DIR/test_files/mock_file.c")"
   expected="$(
     printf "Hello from %s in %s\nHello from %s in %s in multiline comment" \
-      $(realpath "$TESTS_DIR/mock_file.c") $(realpath "$TESTS_DIR") \
-      $(realpath "$TESTS_DIR/mock_file.c") $(realpath "$TESTS_DIR") \
+      $(realpath "$TESTS_DIR/test_files/mock_file.c") $(realpath "$TESTS_DIR/test_files") \
+      $(realpath "$TESTS_DIR/test_files/mock_file.c") $(realpath "$TESTS_DIR/test_files") \
     )"
 
   [ "$result" = "$expected" ]
 }
 
 @test "ccgen command fail" {
-  run ! ccgen "$TESTS_DIR/ccgen_fail.c"
+  run ! ccgen "$TESTS_DIR/test_files/ccgen_fail.c"
 }
